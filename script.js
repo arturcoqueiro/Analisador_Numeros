88b780cc148226778af90ed2a8a996912fd3daa2
Artur Coqueiro <rienstatic@outlook.com>
2022-01-20 08:07:40 -0300

    finalizando script

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var num = document.querySelector('input#num')
 var valores = []
 var out = document.getElementById('vlrs')
+var res = document.getElementById("res")
 
 function ad(){
 
@@ -18,9 +19,49 @@ function ad(){
             it.text = `Valor ${n} Adicionado`
 
             out.appendChild(it)
+            res.innerHTML = ''
         }
     }else{
 
         alert('Informe um Número')
     }
+    num.value = ''
+    num.focus()
 }
+
+function anls(){
+    
+    if (!(valores.length == '')){
+
+        res.innerHTML += `<p>Foram Cadastrados ${valores.length} Valores</p>`
+
+        res.innerHTML += `<p>Maior Valor cadastrado foi ${Math.max.apply(Math, valores)}</p>`
+
+        res.innerHTML += `<p>Menor Valor Cadastrado foi ${Math.min.apply(Math, valores)}</p>`
+
+        var soma = (a, b) => a + b
+
+        res.innerHTML += `<p>Soma dos Valores foi ${valores.reduce(soma)}</p>`
+
+        res.innerHTML += `<p>A média do Valores é ${(valores.reduce(soma)/valores.length).toFixed(1)}</p>`
+    }else{
+
+        alert('Informe um Número')
+    }
+}
+/*
+if (!(valores.length == '')){
+
+    var reset_btn = document.getElementById('reset')
+    var btn = document.createElement('button')
+
+    btn.value = 'Recomeçar'
+    btn.setAttribute('onclick', 'reset()')
+
+    reset_btn.appendChild(btn)
+}
+
+function reset(){
+    valores = ''
+}
+*/
